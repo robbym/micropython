5c9dd90a3ef6408f2081cbc0cd9bfd00fbfc1b7d
= <=>
2020-03-05 15:45:53 -0700

    Improved listener API

--- ports/stm32/machine_listener.c
+++ ports/stm32/machine_listener.c
@@ -28,6 +28,7 @@
 #include <string.h>
 
 #include "py/builtin.h"
+#include "py/misc.h"
 #include "py/mphal.h"
 #include "py/runtime.h"
 #include "py/stream.h"
@@ -35,19 +36,33 @@
 #include "uart.h"
 
 
+#define OUTPUT_BUFFER_SIZE (2048)
+
+typedef struct _listener_obj_t
+{
+    pyb_uart_obj_t *uart;
+    mp_obj_t file;
+    uint8_t *buffer;
+    size_t out_idx;
+} listener_obj_t;
+
 typedef struct _machine_listener_obj_t
 {
     mp_obj_base_t base;
-    pyb_uart_obj_t *uart1;
-    pyb_uart_obj_t *uart2;
-    mp_obj_t file1;
-    mp_obj_t file2;
+    listener_obj_t *listener_list;
+    size_t listener_list_length;
 } machine_listener_obj_t;
 
 const mp_obj_type_t machine_listener_type;
 
-STATIC void process_channel(pyb_uart_obj_t *uart, mp_obj_t file, uint8_t *buffer, size_t buffer_size, uint32_t* outIdx)
+STATIC void process_channel(listener_obj_t *listener)
 {
+    pyb_uart_obj_t *uart = listener->uart;
+    mp_obj_t file = listener->file;
+    uint8_t *buffer = listener->buffer;
+    size_t buffer_size = OUTPUT_BUFFER_SIZE;
+    size_t* out_idx = &listener->out_idx;
+
     int err;
     bool receivedBytes = false;
 
@@ -58,28 +73,28 @@ STATIC void process_channel(pyb_uart_obj_t *uart, mp_obj_t file, uint8_t *buffer
     {
         receivedBytes = true;
 
-        mp_uint_t remaining = buffer_size - *outIdx;
+        mp_uint_t remaining = buffer_size - *out_idx;
 
         mp_uint_t length = uart_rx_any(uart);
         if (length > remaining)
             length = remaining;
 
-        uart_stream->read(uart, &buffer[*outIdx], length, &err);
-        *outIdx += length;
+        uart_stream->read(uart, &buffer[*out_idx], length, &err);
+        *out_idx += length;
 
-        if (*outIdx == buffer_size)
+        if (*out_idx == buffer_size)
         {
-            file_stream->write(file, buffer, *outIdx, &err);
+            file_stream->write(file, buffer, *out_idx, &err);
             file_stream->ioctl(file, MP_STREAM_FLUSH, 0, &err);
-            *outIdx = 0;
+            *out_idx = 0;
         }
     }
 
-    if (!receivedBytes && *outIdx > 0)
+    if (!receivedBytes && *out_idx > 0)
     {
-        file_stream->write(file, buffer, *outIdx, &err);
+        file_stream->write(file, buffer, *out_idx, &err);
         file_stream->ioctl(file, MP_STREAM_FLUSH, 0, &err);
-        *outIdx = 0;
+        *out_idx = 0;
     }
 }
 
@@ -90,61 +105,46 @@ STATIC void machine_listener_print(const mp_print_t *print, mp_obj_t self_in, mp
 STATIC mp_obj_t machine_listener_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
     machine_listener_obj_t *self = m_new_obj(machine_listener_obj_t);
     self->base.type = &machine_listener_type;
-    self->uart1 = NULL;
-    self->uart2 = NULL;
-
-    const char *file1_name = "log1.txt";
-    const char *file2_name = "log2.txt";
-    const char *file_args = "a+";
-
-    mp_obj_t open_args1[2] = {
-        mp_obj_new_str(file1_name, strlen(file1_name)),
-        mp_obj_new_str(file_args, strlen(file_args)),
-    };
-
-    mp_obj_t open_args2[2] = {
-        mp_obj_new_str(file2_name, strlen(file2_name)),
-        mp_obj_new_str(file_args, strlen(file_args)),
-    };
-
-    self->file1 = mp_builtin_open(2, open_args1, (mp_map_t *)&mp_const_empty_map);
-    self->file2 = mp_builtin_open(2, open_args2, (mp_map_t *)&mp_const_empty_map);
-
+    self->listener_list = NULL;
+    self->listener_list_length = 0;
     return MP_OBJ_FROM_PTR(self);
 }
 
-STATIC mp_obj_t machine_listener_add_periph(mp_obj_t self_in, mp_obj_t uart1_in, mp_obj_t uart2_in) {
+STATIC mp_obj_t machine_listener_add(mp_obj_t self_in, mp_obj_t uart_in, mp_obj_t file_in) {
     machine_listener_obj_t *self = MP_OBJ_TO_PTR(self_in);
-    pyb_uart_obj_t *uart1 = MP_OBJ_TO_PTR(uart1_in);
-    pyb_uart_obj_t *uart2 = MP_OBJ_TO_PTR(uart2_in);
-    self->uart1 = uart1;
-    self->uart2 = uart2;
+    pyb_uart_obj_t *uart = MP_OBJ_TO_PTR(uart_in);
+
+    self->listener_list = m_renew(listener_obj_t, self->listener_list, self->listener_list_length, self->listener_list_length + 1);
+    self->listener_list[self->listener_list_length] = (listener_obj_t)
+    {
+        .uart = uart,
+        .file = file_in,
+        .buffer = m_new0(uint8_t, OUTPUT_BUFFER_SIZE),
+        .out_idx = 0,
+    };
+    self->listener_list_length++;
+
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_3(machine_listener_add_periph_obj, machine_listener_add_periph);
+STATIC MP_DEFINE_CONST_FUN_OBJ_3(machine_listener_add_obj, machine_listener_add);
 
-STATIC uint8_t listener_buffer1[2048];
-STATIC uint8_t listener_buffer2[2048];
-STATIC mp_obj_t machine_listener_process(mp_obj_t self_in) {
+STATIC mp_obj_t machine_listener_listen(mp_obj_t self_in) {
     machine_listener_obj_t *self = MP_OBJ_TO_PTR(self_in);
-
-    uint32_t outIdx1 = 0;
-    uint32_t outIdx2 = 0;
 
     for (;;)
     {
-        process_channel(self->uart1, self->file1, listener_buffer1, sizeof(listener_buffer1), &outIdx1);
-        process_channel(self->uart2, self->file2, listener_buffer2, sizeof(listener_buffer2), &outIdx2);
+        for (size_t index = 0; index < self->listener_list_length; index++)
+            process_channel(&self->listener_list[index]);
     }
 
     return mp_const_none;
 }
-STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_listener_process_obj, machine_listener_process);
+STATIC MP_DEFINE_CONST_FUN_OBJ_1(machine_listener_listen_obj, machine_listener_listen);
 
 
 STATIC const mp_rom_map_elem_t machine_listener_locals_dict_table[] = {
-    { MP_ROM_QSTR(MP_QSTR_add_periph), MP_ROM_PTR(&machine_listener_add_periph_obj) },
-    { MP_ROM_QSTR(MP_QSTR_process), MP_ROM_PTR(&machine_listener_process_obj) },
+    { MP_ROM_QSTR(MP_QSTR_add), MP_ROM_PTR(&machine_listener_add_obj) },
+    { MP_ROM_QSTR(MP_QSTR_listen), MP_ROM_PTR(&machine_listener_listen_obj) },
 };
 STATIC MP_DEFINE_CONST_DICT(machine_listener_locals_dict, machine_listener_locals_dict_table);
 
